dc522e3a77d7b60a9e822f0045a4883c9d56fd33
karen <k.valeria.gonzalez@gmail.com>
2021-03-25 15:07:06 -0400

    adds html header, smain (sections) icons

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 const hello = "hello";
 console.log(hello);
+// <i class="fas fa-times-circle"></i> 
+// <i class="fas fa-times"></i>
+// heart icon <i class="fas fa-heart"></i> fill
+// heart icon <i class="far fa-heart"></i> empty
